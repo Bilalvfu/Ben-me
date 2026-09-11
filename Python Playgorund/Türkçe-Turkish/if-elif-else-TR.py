f548e350add88f3b4babada0833079674733fa74
Bilalvfu <bilalhgm09@gmail.com>
2026-09-11 06:27:13 +0300

    Add files via upload

---
+++ Python Playgorund/Türkçe-Turkish/if-elif-else-TR.py
@@ -0,0 +1,12 @@
+isim = input(">")
+yas = int(input(">"))
+
+if isim == 'Ahmet':
+    print('Merhaba Ahmet.')
+elif yas < 12:
+    print('Henüz çocuk yaştasın')
+elif yas > 122:
+    print('Kimse bu kadar yaşamadı.')
+
+else:
+    print('Sen Ahmet de değilsin, çocukta değilsin.')
